499c43fe33aa4e7ae9382780f6a21a46379b9cef
H2M <xushuangzhi1@126.com>
2025-06-29 11:56:46 +0800

    Deployed d2ae4ac with MkDocs version: 1.6.1

---
+++ proj/gpio/gpiopic/rename.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# 定义前缀和后缀
+prefix="gpiopic_20250629_1_"
+suffix=".png"
+
+shopt -s nullglob          # 若没有匹配文件也不报错
+for f in $(ls image-*.png | sort -t'-' -k2 -n); do
+  num=${f#image-}          # 去掉前缀 image-
+  num=${num%.png}          # 去掉后缀 .png
+  new=$((num + 1))         # 递增
+  # 构建新文件名
+  new_name="${prefix}${new}${suffix}"
+
+  # 执行重命名
+  mv "$f" "$new_name"
+
+  # 输出执行信息（可选）
+  echo "Renamed $f to $new_name"
+done
